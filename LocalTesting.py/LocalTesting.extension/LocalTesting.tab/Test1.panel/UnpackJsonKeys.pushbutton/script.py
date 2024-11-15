5e8ecceec0b4f337a7cf80095d8d2de63be0226b
Tuah Hamid <tuah.edu@gmail.com>
2024-11-15 09:51:29 +0800

    commit test

--- LocalTesting.py/LocalTesting.extension/LocalTesting.tab/Test1.panel/UnpackJsonKeys.pushbutton/script.py
+++ LocalTesting.py/LocalTesting.extension/LocalTesting.tab/Test1.panel/UnpackJsonKeys.pushbutton/script.py
@@ -16,10 +16,42 @@ with open(path_value,'r') as schema:
     schema_data_ordered = json.load(schema, object_pairs_hook=OrderedDict) #type: OrderedDict
     schema_data_standard = dict(schema_data_ordered)
 
-for panel, panel_prop in schema_data_ordered.items():
-    print(panel)
-    for level, level_prop in panel_prop.items():
-        print(len(level), level)
-        for key in level_prop.values():
-            print(key)
-    print("\n ")
+# for panel, panel_prop in schema_data_ordered.items():
+#     print(panel)
+#     for level, level_prop in panel_prop.items():
+#         print(len(level), level)
+#         for key in level_prop.values():
+#             print(key)
+#     print("\n ")
+
+panel_data_as_list = schema_data_ordered.values()
+level_names = []
+allow_offset_values = []
+for level, level_prop in panel_data_as_list[0].items():
+    level_names.append(level)
+
+for level in level_names:
+    if len(level) < 10:
+        allow_offset_values.append(0)
+    else:
+        allow_offset_values.append(1)
+
+print(allow_offset_values)
+
+panel_data_as_list = schema_data_ordered.values()
+panel_data_counts = []
+for panel in panel_data_as_list:
+    panel_data_counts.append(len(panel))
+
+panel_unique_counts_set = list(set(panel_data_counts))
+panel_unique_count = len(panel_unique_counts_set)
+
+LEVELCOUNT = panel_unique_counts_set[0]
+
+allow_header_bool_values = []
+for i in range(LEVELCOUNT):
+    if i == 0:
+        allow_header_bool_values.append(1)
+    else:
+        allow_header_bool_values.append(0)
+print(allow_header_bool_values)
